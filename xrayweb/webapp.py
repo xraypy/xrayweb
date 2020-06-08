7c2c4fd3765d284bc3b86b43df50e105feb75fbd
Matthew Newville <newville@cars.uchicago.edu>
2020-06-07 22:07:16 -0500

    simplify

--- xrayweb/webapp.py
+++ xrayweb/webapp.py
@@ -5,28 +5,18 @@ from flask import request, session
 
 import xraydb
 
-db = xraydb.get_xraydb()
 
 app = Flask('xrayweb', static_folder='static')
 app.config.from_object(__name__)
 
-
 @app.route('/element/', methods=['GET', 'POST'])
 @app.route('/element/<elem>',  methods=['GET', 'POST'])
 def element(elem=None):
-
-    global db 
     edges = {}
     if elem is not None:
-        edges= db.xray_edges(elem)
-    print("ELEMENT ", db, edges)
+        edges= xraydb.xray_edges(elem)
     return render_template('elements.html', edges=edges, elem=elem)
 
 @app.route('/')
 def index():
     return redirect(url_for('element'))
-
-
-if __name__ == "__main__":
-    app.jinja_env.cache = {}
-    app.run(port=PORT)
