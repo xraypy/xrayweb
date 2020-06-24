e359818afb3e7b0616d64c7a021b3f3d55cc0d4b
anicolellis <anicolellis1@gmail.com>
2020-06-24 16:28:24 -0500

    Added formulas page, first steps

--- xrayweb/webapp.py
+++ xrayweb/webapp.py
@@ -12,11 +12,20 @@ app.config.from_object(__name__)
 @app.route('/element/', methods=['GET', 'POST'])
 @app.route('/element/<elem>',  methods=['GET', 'POST'])
 def element(elem=None):
-    edges = atomic = {}
+    edges = atomic = lines = {}
     if elem is not None:
         edges= xraydb.xray_edges(elem)
-        atomic= {'n': xraydb.atomic_number(elem), 'mass': xraydb.atomic_mass(elem)}
-    return render_template('elements.html', edges=edges, elem=elem, atomic=atomic)
+        atomic= {'n': xraydb.atomic_number(elem), 'mass': xraydb.atomic_mass(elem), 'density': xraydb.atomic_density(elem)}
+        lines= xraydb.xray_lines(elem)
+    return render_template('elements.html', edges=edges, elem=elem, atomic=atomic, lines=lines)
+
+@app.route('/formula/', methods=['GET', 'POST'])
+@app.route('/formula/<fmla>', methods=['GET', 'POST'])
+def formula(fmla=None):
+    if fmla is not None:
+        pass
+        #obtain info from the database
+    return render_template('formulas.html')
 
 @app.route('/')
 def index():
