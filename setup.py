a8929e66333261706ddcacd938bffd24e9d37ce2
Matthew Newville <newville@cars.uchicago.edu>
2024-11-02 11:35:36 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -2,31 +2,3 @@
 from setuptools import setup
 if __name__ == "__main__":
     setup()
-
-#
-# import os
-#
-# version = '0.1'
-# with open(os.path.join('xrayweb', '_version.py')) as fh:
-#     for line in fh.readlines():
-#         line = line[:-1].strip()
-#         if line.startswith('__version'):
-#             words = line.split('=') + [' ', ' ']
-#             version = words[1].strip().replace("'", '').replace('"', '')
-#
-#
-# pkg_data = {'xrayweb.template': ['template/*'],
-#             'xrayweb.static': ['static/*']}
-#
-# setup(name='xrayweb',
-#       version = version,
-#       author       = 'Alex Nicolellis and Matthew Newville',
-#       author_email = 'newville@cars.uchicago.edu',
-#       license      = 'MIT',
-#       description  = 'Web interface for X-ray Properties of the elements',
-#       install_requires=('sqlalchemy', 'numpy', 'scipy', 'xraydb', 'flask', 'plotly'),
-#       package_dir = {'xrayweb': 'xrayweb'},
-#       packages = ['xrayweb'],
-#       package_data = pkg_data,
-#       zip_safe=False,
-# )
