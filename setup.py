f39e73dfd1574660accac31ffd6a502d0178cea2
Matthew Newville <newville@cars.uchicago.edu>
2024-11-02 11:21:05 -0500

    use pyproject and setuptools-scm

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python
 from setuptools import setup
-import os
+if __name__ == "__main__":
+    setup()
 
-version = '0.1'
-with open(os.path.join('xrayweb', '_version.py')) as fh:
-    for line in fh.readlines():
-        line = line[:-1].strip()
-        if line.startswith('__version'):
-            words = line.split('=') + [' ', ' ']
-            version = words[1].strip().replace("'", '').replace('"', '')
-
-
-pkg_data = {'xrayweb.template': ['template/*'],
-            'xrayweb.static': ['static/*']}
-
-setup(name='xrayweb',
-      version = version,
-      author       = 'Alex Nicolellis and Matthew Newville',
-      author_email = 'newville@cars.uchicago.edu',
-      license      = 'MIT',
-      description  = 'Web interface for X-ray Properties of the elements',
-      install_requires=('sqlalchemy', 'numpy', 'scipy', 'xraydb', 'flask', 'plotly'),
-      package_dir = {'xrayweb': 'xrayweb'},
-      packages = ['xrayweb'],
-      package_data = pkg_data,
-      zip_safe=False,
-)
+#
+# import os
+#
+# version = '0.1'
+# with open(os.path.join('xrayweb', '_version.py')) as fh:
+#     for line in fh.readlines():
+#         line = line[:-1].strip()
+#         if line.startswith('__version'):
+#             words = line.split('=') + [' ', ' ']
+#             version = words[1].strip().replace("'", '').replace('"', '')
+#
+#
+# pkg_data = {'xrayweb.template': ['template/*'],
+#             'xrayweb.static': ['static/*']}
+#
+# setup(name='xrayweb',
+#       version = version,
+#       author       = 'Alex Nicolellis and Matthew Newville',
+#       author_email = 'newville@cars.uchicago.edu',
+#       license      = 'MIT',
+#       description  = 'Web interface for X-ray Properties of the elements',
+#       install_requires=('sqlalchemy', 'numpy', 'scipy', 'xraydb', 'flask', 'plotly'),
+#       package_dir = {'xrayweb': 'xrayweb'},
+#       packages = ['xrayweb'],
+#       package_data = pkg_data,
+#       zip_safe=False,
+# )
--- xrayweb/__init__.py
+++ xrayweb/__init__.py
@@ -1,3 +1,2 @@
-from ._version import __version__
+from .version import __version__
 from .webapp import app
-
